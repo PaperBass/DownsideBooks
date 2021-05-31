e981daae7b293940e5dfec58118466a9660ced4e
Nigel Meyer <nigelmeyer42@gmail.com>
2021-05-31 12:14:01 -0500

    added menu tab, hide links, reveal cancel button

---
+++ apps.js
@@ -0,0 +1,20 @@
+function displayLinks() {
+    var y = document.getElementById('bottomNavMenu');
+    var x = document.getElementById('myLinks');
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.display = "block";
+    } else {
+        x.style.display = "block";
+        y.style.display = "none";
+    }
+}
+
+// function toggleMenu() {
+//     var x = document.getElementById('bottomNavMenu');
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
