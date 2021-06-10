e2be1490b4d2b8bb2aa115675e6b22369d6f8b26
Nigel Meyer <nigelmeyer42@gmail.com>
2021-06-10 00:09:12 -0400

    fixed banner position

--- apps.js
+++ apps.js
@@ -22,6 +22,24 @@ function bottomLinks() {
     }
 }
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("titleRow");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 // function toggleMenu() {
 //     var x = document.getElementById('bottomNavMenu');
 //     if (x.style.display === "block") {
